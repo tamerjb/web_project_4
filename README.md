0acf7c3f4720036960e3e3afb69593940e42025a
Tamer Jb <tamerjb.98@gmail.com>
2022-04-02 13:13:20 +0300

    refactor:removed unnecessary code lines v2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Overview
 
-a project that showcases a trip to the US by a particular traveller. All the elements are displayed correctly on some screen sizes. The main skill learned for this project is DOM  with Javascript. you can edit the name and the title 
+a project that showcases a trip to the US by a particular traveller. All the elements are displayed correctly on some screen sizes. The main skill learned for this project is DOM  with Javascript. you can edit the name and the title
 #### Design File Used:
 
 - [Link to the project in Figma](https://www.figma.com/file/SurN1jaeEQIhuZEDMhmWWf/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
@@ -19,7 +19,7 @@ a project that showcases a trip to the US by a particular traveller. All the ele
 
 # Github Pages Link:
 
-- [Link to the github pages link](https://github.com/tamerjb/web_project_4.git)
+- [Link to the github pages link](https://tamerjb.github.io/web_project_4/)
 
 ## Plan on improving the project
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
 let form = document.querySelector(".form");
 let profile = document.querySelector(".profile");
-let saveButton = form.querySelector(".form__button");
 let profileName = profile.querySelector(".profile__name");
 let editProfileButton = profile.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let closeButton = popup.querySelector(".popup__button-close");
-let cards = document.querySelector(".cards");
 let profileTitle = profile.querySelector(".profile__title");
-let likebuttons = cards.querySelectorAll(".card__like-button");
 let inputName = form.querySelector(".form__input[name='name']");
 let inputTitle = form.querySelector(".form__input[name='title']");
+
+
 //functions
 function openForm() {
   inputName.value = profileName.textContent;
@@ -25,19 +24,25 @@ function formHandler(event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
   profileTitle.textContent = inputTitle.value;
+  closeForm();
 }
 
 //eventListener
 editProfileButton.addEventListener("click", openForm);
 closeButton.addEventListener("click", closeForm);
-saveButton.addEventListener("click", closeForm);
 form.addEventListener("submit", formHandler);
 
-for (let i = 0; i < likebuttons.length; i++) {
-  let likeButton = likebuttons[i];
-  function toggleLike() {
-    likeButton.classList.toggle("card__like-button_active");
-  }
-  likeButton.addEventListener("click", toggleLike);
-}
+// saveButton.addEventListener("click", closeForm);
+
+// let saveButton = form.querySelector(".form__button");
+// let cards = document.querySelector(".cards");
+// let likebuttons = cards.querySelectorAll(".card__like-button");
+
+// for (let i = 0; i < likebuttons.length; i++) {
+//   let likeButton = likebuttons[i];
+//   function toggleLike() {
+//     likeButton.classList.toggle("card__like-button_active");
+//   }
+//   likeButton.addEventListener("click", toggleLike);
+// }
 
