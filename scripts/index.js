15c5de4b68bca688e3867a19953b2fd1f9bd9a6a
Tamer Jb <tamerjb.98@gmail.com>
2022-04-20 18:05:33 +0300

    feat:added JavaScript code (add/delete/preview/like) for photos

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+///////////////////////////////////////////
+//////// Selectors ///////////
+///////////////////////////////////////////
 let form = document.querySelector(".form");
 let profile = document.querySelector(".profile");
 let profileName = profile.querySelector(".profile__name");
@@ -8,37 +11,90 @@ let profileTitle = profile.querySelector(".profile__title");
 let inputName = form.querySelector(".form__input[name='name']");
 let inputTitle = form.querySelector(".form__input[name='title']");
 const cards = document.querySelector(".cards");
+
+
 const cardlist = cards.querySelector(".cards__list");
+const addForm = profile.querySelector(".profile__add-button");
+
+const placeForm = document.querySelector(".popup__form_type_add-place");
+const placeName = placeForm.querySelector(".form__input_type_place-name");
+const placeURL = placeForm.querySelector(".form__input_type_place-url");
+
+const imgPrev = document.querySelector(".popup_type_image-prev");
+const imgPrevCloseButton = imgPrev.querySelector(
+  ".popup__close-button_preview"
+);
+
+const placeAdd = document.querySelector(".popup_type_add-place");
+const placeClose = placeAdd.querySelector(".popup__button-close_type_place");
 
 
 
-
-//functions
-function openForm() {
+///////////////////////////////////////////
+//////// Functions ///////////
+///////////////////////////////////////////
+function EditNameForm() { //toggle name form
   inputName.value = profileName.textContent;
   inputTitle.value = profileTitle.textContent;
-  popup.classList.add("popup_opened");
+  popup.classList.toggle("popup_opened");
 }
 
-function closeForm() {
-  popup.classList.remove("popup_opened");
+function toggleClass(item, x) { //function toggles(add/remove) a specifc class, *item* is selcetor name / x is the class that want to add/remove .
+  item.classList.toggle(x);
 }
 
-function formHandler(event) {
+function formHandler(event) { //saves the name after editing
   event.preventDefault();
   profileName.textContent = inputName.value;
   profileTitle.textContent = inputTitle.value;
-    popup.classList.remove("popup_opened");
-
+  popup.classList.remove("popup_opened");
 }
 
-//eventListener
-editProfileButton.addEventListener("click", openForm);
-closeButton.addEventListener("click", closeForm);
-form.addEventListener("submit", formHandler);
+function addCard(event) { //this function to add the card manullay
 
-const initialCards = [
-  {
+  event.preventDefault();
+
+  renderCard({
+    name: placeName.value,
+    link: placeURL.value
+  }, cardlist);
+
+  toggleClass(placeAdd, "popup_opened");
+  placeForm.reset();
+}
+
+function renderCard(card, list) { // adds the card to the first place
+  list.prepend(createcard(card));
+}
+
+function previewImage(card) {
+  const popupImage = imgPrev.querySelector(".popup__image");
+  const popupCaption = imgPrev.querySelector(".popup__caption");
+  popupImage.src = card.link;
+  popupImage.alt = `A beautiful place in ${card.name}`;
+  popupCaption.textContent = card.name;
+  // ToggleAddPopup(imgPrev);
+  toggleClass(imgPrev, "popup_opened");
+}
+
+
+///////////////////////////////////////////
+//////// EventListeners ///////////
+///////////////////////////////////////////
+
+editProfileButton.addEventListener("click", EditNameForm);
+closeButton.addEventListener("click", EditNameForm);
+form.addEventListener("submit", formHandler);
+addForm.addEventListener("click", () => toggleClass(placeAdd, "popup_opened")); //open photo add form
+placeClose.addEventListener("click", () => toggleClass(placeAdd, "popup_opened")); // close add form
+placeForm.addEventListener("submit", addCard) //this listining to event submit (save)
+imgPrevCloseButton.addEventListener("click", () => toggleClass(imgPrev, "popup_opened"));
+
+
+
+
+
+const initialCards = [{
     name: "Yosemite Valley",
     link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   },
@@ -64,7 +120,7 @@ const initialCards = [
   },
 ];
 //create card
-function createcard(info){
+function createcard(info) {
   const cardtemplate = document.querySelector("#card-template").content;
   const cardelement = cardtemplate.querySelector(".card").cloneNode(true);
   const cardtitle = cardelement.querySelector(".card__info-title");
@@ -75,22 +131,17 @@ function createcard(info){
   cardimage.src = info.link;
   cardimage.alt = `place in ${info.name}`;
 
-  cardimage.addEventListener("click", () => previewimage(info));
+  cardimage.addEventListener("click", () => previewImage(info));
   deletebutton.addEventListener("click", () => cardelement.remove());
   likebutton.addEventListener("click", () =>
-    toggleClass(likebutton, "card__like-button_active")
-
-  );
+    toggleClass(likebutton, "card__like-button_active"));
   return cardelement;
 }
 //funcation toggles class on/off
-function toggleClass(item,x) {
-  item.classList.toggle(x);
-}
+
 //adds the card to the list
 function addcard(card, list) {
   list.prepend(createcard(card));
 }
 //adding the array
 initialCards.forEach((card) => addcard(card, cardlist));
-
