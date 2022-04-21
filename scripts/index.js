e6ec54f29fc29702a667c54b2549cf39ca41f01f
Tamer Jb <tamerjb.98@gmail.com>
2022-04-21 14:20:22 +0300

    deleted extra selectors html/js

--- scripts/index.js
+++ scripts/index.js
@@ -25,7 +25,7 @@ const imgPrevCloseButton = imgPrev.querySelector(
   ".popup__close-button"
 );
 
-const placeAdd = document.querySelector(".popup-add-place");
+const placeAdd = document.querySelector(".place__add");
 const placeClose = placeAdd.querySelector(".popup__button-close-type-place");
 
 
