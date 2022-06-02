873237128bcae6bd19b69934d48840b04d7ecfc0
Tamer Jb <tamerjb.98@gmail.com>
2022-06-02 13:47:10 +0300

    renamed the card file - fixed the resetvalidtion for the button

---
+++ scripts/Card.js
@@ -0,0 +1,47 @@
+import {
+    previewImage
+} from "./index.js";
+
+export class Card {
+    constructor(data, selector) {
+        this._text = data.name;
+        this._link = data.link;
+        this._selector = selector;
+    }
+    _getCloneFromTemplate() {
+        const cardElement = document
+            .querySelector(this._selector)
+            .content.querySelector(".card")
+            .cloneNode(true);
+        return cardElement;
+    }
+    _handleLikeButton(evt) {
+        evt.target.classList.toggle("card__like-button_active");
+    }
+    _setEventListeners() {
+
+        this._likeButton.addEventListener("click", (evt) => {
+            this._handleLikeButton(evt);
+        });
+        this._image.addEventListener("click", (evt) => {
+            previewImage(this._link, this._text);
+        });
+        this._deleteButton.addEventListener("click", (evt) => {
+            this._element.remove();
+
+        })
+    }
+    generateCard() {
+        this._element = this._getCloneFromTemplate();
+        this._image = this._element.querySelector(".card__image");
+        this._likeButton = this._element.querySelector(".card__like-button");
+        this._deleteButton = this._element.querySelector(".card__image-trash");
+        this._element.querySelector(".card__info-title").textContent = this._text;
+        this._image.src = this._link;
+        this._image.alt = `Place in ${this._text}`;
+
+        this._setEventListeners();
+
+        return this._element;
+    }
+}
