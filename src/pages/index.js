c3407d46f8c013cc287941f6ff2cebd2e84da332
Tamer Jb <tamerjb.98@gmail.com>
2022-06-17 14:25:15 +0300

    deleted extra code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,3 @@
-// import {
-
-//   openPopup,
-//   closePopup,
-//   closeOnEscape,
-//   closeOnClickOutside,
-
-// }
-// from "../scripts/utils.js";
-
 import {
   FormValidator
 }
@@ -70,19 +60,7 @@ import {
 ///////////////////////////////////////////
 //////// EventListeners ///////////////////
 ///////////////////////////////////////////
-// closeButtons.forEach((button) => { //close popups by pressing on X
-//   const popup = button.closest('.popup');
-//   button.addEventListener('click', () => closePopup(popup));
-// });
 
-// editProfileButton.addEventListener("click", () => openProfileForm()); // edit profile open popup
-// addCardPopup.addEventListener("click", () => openPopup(placeForm)); //open the add photo form.
-
-
-// profileEdit.addEventListener("submit", handleProfileFormSubmit);
-// // saves the profile info + prevents the site submit .
-// placePopupForm.addEventListener("submit", addCard);
-// //this listining to event submit (save)
 editProfileButton.addEventListener("click", () => {
   const info = userInfo.getUserInfo();
   inputName.value = info.name;
@@ -122,57 +100,10 @@ const userInfo = new UserInfo({
   jobSelector: ".profile__title",
 });
 
+///////////////////////////////////////////
+//////// Popup creation ////////////////
+/////////////////////////////////////////
 
-
-
-
-
-/** This line adds the array to the page */
-
-// initialCards.forEach((card) => renderCard(card, cardList));
-
-// function openProfileForm() { //opens the edit name form
-//   fillProfileForm();
-//   openPopup(profilePopup);
-
-// }
-
-// function fillProfileForm() { //this function set the input fields value.
-//   inputName.value = profileName.textContent;
-//   inputTitle.value = profileTitle.textContent;
-// }
-
-// function saveProfileForm() {
-//   profileName.textContent = inputName.value;
-//   profileTitle.textContent = inputTitle.value;
-//   closePopup(profilePopup);
-
-// }
-
-
-// function toggleClass(item, className) { //function toggles(add/remove) a specifc class, *item* is selcetor name / x is the class that want to add/remove .
-//   item.classList.toggle(className);
-// }
-
-// function handleProfileFormSubmit(event) { //saves the name after editing
-//   event.preventDefault();
-//   saveProfileForm();
-// }
-
-// function addCard(event) { //this function to add the card manullay
-//   event.preventDefault();
-//   renderCard({
-//     name: placeName.value,
-//     link: placeURL.value
-//   }, cardList);
-
-//   // toggleClass(placeForm, "popup_opened");
-//   closePopup(placeForm);
-//   placePopupForm.reset();
-//   addFormValidator.resetValidation();
-
-
-// }
 
 
 const imagePopup = new PopupWithImage(".popup-prev");
@@ -189,18 +120,9 @@ const addPopup = new PopupWithForm(".popup-place", (data) => {
 });
 addPopup.setEventListeners();
 
-// function renderCard(card, list) { // adds the card to the first place
-//   const cardToRender = new Card(card, cardTemplateSelector).generateCard();
-//   list.prepend(cardToRender);
-// }
-
-// export function previewImage(thisLink, thisName) {
-
-//   popupPreviewImg.src = thisLink;
-//   popupPreviewImg.alt = `A beautiful place in ${thisName}`;
-//   popupPreviewCaption.textContent = thisName;
-//   openPopup(imgPreview);
-// }
+///////////////////////////////////////////
+//////// card creation ////////////////
+/////////////////////////////////////////
 const placesSection = new Section({
     items: initialCards,
     renderer: (data) => renderCard(data),
